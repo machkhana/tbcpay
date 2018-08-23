182eb8569f623deaf86f8ba0971915d15cb752b5
giorgi machkhaneli <g.machkhaneli@gmail.com>
2018-08-23 10:53:46 +0400

    Create post.php

---
+++ post.php
@@ -0,0 +1,4 @@
+<form action="payment.php" method="GET">
+  <p><input type="text" name="amnt" class="payinput" id="amnt" value="20" ></p>
+  <input type="submit" title="Pay" class="paybutton" value="გადარიცხვა">  
+</form>
